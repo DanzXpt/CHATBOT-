31647e13419cfb0c7b2810ff96aaa3d2ceca4490
DannzXxxx <dannzxxxx@gmail.com>
2026-05-11 22:43:58 +0700

    use lighter gemini model

--- app.py
+++ app.py
@@ -38,8 +38,7 @@ if not api_key:
 # SETUP GEMINI
 # =========================
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("models/gemini-2.5-flash")
-
+model = genai.GenerativeModel("models/gemini-3.1-flash-lite")
 # =========================
 # FUNCTIONS
 # =========================
